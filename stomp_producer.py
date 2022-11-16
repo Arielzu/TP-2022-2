d39d32c0c647f00114d4ba7177a6c1fd81226056
aazuniga <aazuniga@inf.utfsm.cl>
2022-11-15 22:38:26 -0300

    fix producer

--- stomp_producer.py
+++ stomp_producer.py
@@ -8,14 +8,14 @@ conn = stomp.Connection()
 conn.connect('admin', 'password', wait=True)
 while True:
     #niveles de glicemia normales(mg/dl) 90-130
-    glicemia = str(random.randint(70,200))
+    glicemia = random.randint(70,200)
     if glicemia < 90:
-        glicemia += " - Bajo"
+        glicemia = str(glicemia) + " - Bajo"
     elif glicemia > 130:
-        glicemia += " - Alto"
+        glicemia = str(glicemia) + " - Alto"
     else:
-        glicemia += " - Normal"
+        glicemia = str(glicemia) + " - Normal"
 
     conn.send(body=' '.join(glicemia), destination="/queue/test")
-    time.sleep(2)
+    time.sleep(5)
 conn.disconnect()
